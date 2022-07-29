512ed90fd6e8f1ec8abafc65c4f87819de2b92dc
Andrew Sykes <andrewsykes@btinternet.com>
2022-07-29 09:48:46 +0100

    lightbox

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,6 +1,136 @@
-const lightbox = document.querySelector('.lightbox'),
-      invokeMethod = (o, m, tgt) => o[m](tgt);
+ function doPartial(flag) {
+    return function p(f, ...vs) {
+      if (f.length === vs.length) {
+        return flag ? () => f(...vs) : f(...vs);
+      }
+      return (...rest) => p(f, ...vs, ...rest);
+    };
+  }
+
+  function curryFactory(dir) {
+    let defer = fun => a => () => fun(a),
+      right = function(config) {
+        if (!config || !config.defer || !config.deflt) {
+          return defer;
+        }
+        let options = {
+          deflt: {
+            two: fun => b => a => fun(a, b),
+            three: fun => c => b => a => fun(a, b, c),
+            four: fun => d => c => b => a => fun(a, b, c, d)
+          },
+          defer: {
+            two: fun => b => a => () => fun(a, b),
+            three: fun => c => b => a => () => fun(a, b, c),
+            four: fun => d => c => b => a => () => fun(a, b, c, d)
+          }
+        };
+        return config.defer ? options.defer[config.defer] : options.deflt[config.deflt];
+      },
+      left = function(config) {
+        if (!config || !config.defer || !config.deflt) {
+          return defer;
+        }
+        let options = {
+          deflt: {
+            two: fun => a => b => fun(a, b),
+            three: fun => a => b => c => fun(a, b, c),
+            four: fun => a => b => c => d => fun(a, b, c, d)
+          },
+          defer: {
+            two: fun => a => b => () => fun(a, b),
+            three: fun => a => b => c => () => fun(a, b, c),
+            four: fun => a => b => c => d => () => fun(a, b, c, d)
+          }
+        };
+        return config.defer ? options.defer[config.defer] : options.deflt[config.deflt];
+      };
+    return dir === 'right' ? right : dir === 'left' ? left : defer;
+  }
+
+function wrap (f, o, ...vs){
+        f.apply(o, [o, vs]);
+        return o;
+      }
+
+function doReturn (ptl, o){
+    ptl(o);
+    return o;
+      }
+
+function doInvoke (ptl, v){
+    return ptl(v);
+      }
 
 
+ if(typeof Function.prototype.wrap ==='undefined'){          
+        Function.prototype.wrap = function(wrapper, ..._vs) {
+            let _method = this;//the function
+            return function(...vs) {
+                return wrapper.apply(this, [_method.bind(this), ..._vs, ...vs]);
+            };
+        };
+    }
 
-lightbox.addEventListener('click', (e) => e.preventDefault());
+function remove() {
+    this.parentNode.removeChild(this);
+}
+
+function F(m, ...funs){
+    return function(o){
+        return funs[m]((f) => f(o));
+    };
+}
+
+function G(m, ...funs){
+    return function(array){
+        return array[m]((v, i) => funs[i](v));
+    };
+}
+
+
+const deferPTL = doPartial(true),
+      ptL = doPartial(),
+      con = (v) => console.log(v),
+      compose = (...fns) => fns.reduce((f, g) => (...vs) => f(g(...vs))),
+      getter = (o, p) => o[p],
+      curry2 = fun => b => a => fun(a, b),
+      curry3 = fun => c => b => a => fun(a, b, c),
+      lightbox = document.querySelector('.lightbox'),
+      invoke = (f, v) => f(v),
+      invokeMethod = (o, m, v) => o[m](v),
+      //invokeMethodWrap = invokeMethod.wrap(doInvoke),
+      applyMethod = (o, m, v) => o[m].apply(o, [v]),
+      lazyInvoke = (m, v, o) => o[m](v),
+      lazyInvoke2 = (m, p, o, v) => o[m](p, v),
+      lazyInvoke3 = (m, p, v, o) => o[m](p, v),
+      doMake = deferPTL(invokeMethod, document, 'createElement'),
+      getClassList = curry2(getter)('classList'),
+      getTarget = curry2(getter)('target'),
+      //getAttribute = curry3(invokeMethod)('src')('getAttribute'),
+      getAttribute = ptL(lazyInvoke, 'getAttribute'),
+      setAttribute = ptL(lazyInvoke3, 'setAttribute'),
+      addListener = curry2(ptL(lazyInvoke2, 'addEventListener', 'click'))(remove),
+      setSrc = curry2(setAttribute('src')),
+      setAlt = curry2(setAttribute('alt')),
+      doOverlay = deferPTL(lazyInvoke, 'add', 'overlay'),
+      doDiv = doMake('div'),
+      doFig = doMake('figure'),
+      doLink = doMake('a'),
+      doImg = doMake('img'),
+      doCaption = doMake('figcaption'),
+      doRender = ptL(invokeMethod, document.body, 'appendChild'),
+      doTest = ptL(invokeMethod, console, 'log'),
+     // makeDiv = compose(doOverlay, getClassList, doRender, doDiv),
+      makeDiv = compose(doRender, doDiv),
+      getSrc = compose(getAttribute('src'), getTarget),
+      getAlt = compose(getAttribute('alt'), getTarget),
+      git = F('map', getAttribute('src'), getAttribute('alt')),
+      sit = G('map', setAttribute('src'), setAttribute('alt')),
+      doGit = compose(sit, git, getTarget);
+
+
+lightbox.addEventListener('click', (e) => {
+    e.preventDefault();    
+    doGit(e)[1](makeDiv())
+});
