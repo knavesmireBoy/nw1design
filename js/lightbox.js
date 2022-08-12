0b32d8d6b07477e1505ceb57c6af7ded92185b93
Andrew Sykes <andrewsykes@btinternet.com>
2022-08-12 07:00:21 +0100

    lightBox

--- js/lightbox.js
+++ js/lightbox.js
@@ -51,11 +51,6 @@
      return vals[m]((v, i) => funs[i](v));
  }
 
- function F(o, p, m, v) {
-     o = getResult(o);
-     return o[p][m](v);
- }
-
  function find(reg) {
      return function (node) {
          while (!node.nodeName.match(reg)) {
@@ -65,31 +60,22 @@
      return node;
  }
 
- function doWhen(pred, action, v) {
-     if (pred(v)) {
-         return action(v);
-     }
- }
-
 
  const deferPTL = doPartial(true),
      ptL = doPartial(),
-     con = (v) => console.log(v),
+     //con = (v) => console.log(v),
      compose = (...fns) => fns.reduce((f, g) => (...vs) => f(g(...vs))),
      getter = (o, p) => o[p],
      curry2 = fun => b => a => fun(a, b),
-     curry3 = fun => c => b => a => fun(a, b, c),
      lightbox = document.querySelector('.lightbox'),
      invoke = (f, v) => f(v),
      invokeMethod = (o, m, v) => o[m](v),
      lazyInvoke2 = (m, p, o, v) => o[m](p, v),
      invokeMethodBridge = (m, v, o) => {
-         o = getResult(o);
-         return invokeMethod(o, m, getResult(v));
+         return invokeMethod(getResult(o), m, v);
      },
      invokeMethodBridgeCB = (cb) => (m, v, o) => {
-         o = cb(o);
-         return invokeMethod(o, m, v);
+         return invokeMethod(cb(o), m, v);
      },
 
      getParent = curry2(getter)('parentNode'),
@@ -98,17 +84,14 @@
      doMakeNow = ptL(invokeMethod, document, 'createElement'),
      getClassList = curry2(getter)('classList'),
      getTarget = curry2(getter)('target'),
-     matcher = compose(curry3(invokeMethod)(/img/i)('match'), curry2(getter)('nodeName'), getTarget),
      getAttribute = ptL(invokeMethodBridge, 'getAttribute'),
      getParentAttribute = ptL(invokeMethodBridgeCB(getParent), 'getAttribute'),
      setAttribute = ptL(lazyInvoke2, 'setAttribute'),
-     invokeThen = invokeMethodBridge.wrap(doReturn),
      addListener = curry2(ptL(lazyInvoke2, 'addEventListener', 'click'))(remove),
      setSrc = curry2(setAttribute('src')),
      setAlt = curry2(setAttribute('alt')),
      doOverlay = ptL(invokeMethodBridge, 'add', 'overlay'),
      doDiv = doMake('div'),
-     doLink = doMake('a'),
      doImg = doMake('img'),
      prepend = curry2(ptL(invokeMethodBridge, 'appendChild')),
      append = ptL(invokeMethodBridge, 'appendChild'),
@@ -116,19 +99,15 @@
      doRender = prepend(document.body),
      /*puzzled as to why figure was being created with an extra image on evry click
      the argument to prepend was an element and was being extended on evry call, needs a fresh instance per click*/
-     //doFig = prepend(doMakeNow('figure')), 
+     //doFig = prepend(doMakeNow('figure')),
      doFig = prepend(doMake('figure')),
      doCap = append(doMakeNow('figcaption')),
-     doTest = ptL(invokeMethod, console, 'log'),
+     //doTest = ptL(invokeMethod, console, 'log'),
      makeDiv = compose(doRender, doDiv),
-     findNode = find(/figure/i),
-     getHref = getAttribute('href'),
      git = ptL(FF, 'map', [getParentAttribute('href'), getAttribute('alt')]),
      sit = ptL(zip, 'map', [setSrc, setAlt]),
      enhance = compose(doOverlay, getClassList).wrap(doReturn),
-     doGit = compose(enhance, getParent, curry2(append)(makeDiv), addListener.wrap(doReturn), getParent, getParent, doClose, doCap, getParent, doFig, curry2(invoke)(doImg), ptL(EE, 'forEach'), sit, git, getTarget),
-     doGit1 = compose(addListener, enhance, getParent, curry2(append)(makeDiv), curry2(invoke)(doImg), ptL(EE, 'forEach'), sit, git, getTarget);
-
+     doGit = compose(enhance, getParent, curry2(append)(makeDiv), addListener.wrap(doReturn), getParent, getParent, doClose, doCap, getParent, doFig, curry2(invoke)(doImg), ptL(EE, 'forEach'), sit, git, getTarget);
  lightbox.addEventListener('click', (e) => {
      e.preventDefault();
      e.stopPropagation();
