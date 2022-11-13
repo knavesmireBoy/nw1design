e664fe40a40ae0bfd0a95bc153f563b8f7a8bd0f
Andrew Sykes <andrewsykes@btinternet.com>
2022-11-13 18:19:54 +0000

    Finder

---
+++ js/finder.js
@@ -0,0 +1,48 @@
+/*jslint nomen: true */
+/*global window: false */
+/*global Publisher: false */
+/*global nW1: false */
+/* eslint-disable indent */
+
+
+if (!window.nW1) {
+    window.nW1 = {};
+  }
+
+class Finder extends Publisher {
+    constructor(grp = [], kls = "active", h = []) {
+      super(h);
+      this.grp = grp;
+      this.current = null;
+      this.finder = () => null;
+    }
+    hide(el, click) {
+      if (el && el === this.current && click) {
+        this.current = null;
+        return nW1.ops.undoActive(el);
+      }
+    }
+    show(el, click) {
+      if (el && !this.hide(el, click)) {
+        nW1.ops.undoActiveCB(this.grp);
+        this.current = nW1.ops.doActive(el);
+      }
+      return this.current;
+    }
+    setFinder(src) {
+      this.finder = Finder.doFinder(src);
+      return this.search();
+    }
+    search() {
+      throw new Error("must be implemented in a subclass");
+    }
+    static from(grp, kls = "active") {
+      return new Finder(grp, kls);
+    }
+    static doFinder(str) {
+      return function (cur) {
+        //becomes this.finder callback to findIndex
+        return str.match(/\/(\w+)_/.exec(cur)[1]);
+      };
+    }
+  }//finder
--- js/navigation.js
+++ js/navigation.js
@@ -179,43 +179,7 @@
       }
     };
   }
-  class Finder extends Publisher {
-    constructor(grp = [], kls = "active", h = []) {
-      super(h);
-      this.grp = grp;
-      this.current = null;
-      this.finder = () => null;
-    }
-    hide(el, click) {
-      if (el && el === this.current && click) {
-        this.current = null;
-        return ops.undoActive(el);
-      }
-    }
-    show(el, click) {
-      if (el && !this.hide(el, click)) {
-        ops.undoActiveCB(this.grp);
-        this.current = ops.doActive(el);
-      }
-      return this.current;
-    }
-    setFinder(src) {
-      this.finder = Finder.doFinder(src);
-      return this.search();
-    }
-    search() {
-      throw new Error("must be implemented in a subclass");
-    }
-    static from(grp, kls = "active") {
-      return new Finder(grp, kls);
-    }
-    static doFinder(str) {
-      return function (cur) {
-        //becomes this.finder callback to findIndex
-        return str.match(/\/(\w+)_/.exec(cur)[1]);
-      };
-    }
-  }//finder
+
   let $painter = null,
     throttlePause,
     getDesktop = nW1.ops.pApply(Modernizr.mq, ipad);
