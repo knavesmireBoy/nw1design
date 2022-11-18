7d20ae30cb200941f8a3edfdb58ae1d3d73160e2
Andrew Sykes <andrewsykes@btinternet.com>
2022-11-18 17:31:46 +0000

    lightbox validation

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,108 +1,112 @@
 /*jslint nomen: true */
+/*global nW1: false */
 /* eslint-disable indent */
 
- function doPartial(flag) {
-     return function p(f, ...vs) {
-         if (f.length === vs.length) {
-             return flag ? () => f(...vs) : f(...vs);
-         }
-         return (...rest) => p(f, ...vs, ...rest);
-     };
- }
+if (!window.nW1) {
+  window.nW1 = {};
+}
 
- function doReturn(ptl, o) {
-     ptl(o);
-     return o;
- }
- if (typeof Function.prototype.wrap === 'undefined') {
-     Function.prototype.wrap = function (wrapper, ..._vs) {
-         let _method = this; //the function
-         return function (...vs) {
-             return wrapper.apply(this, [_method.bind(this), ..._vs, ...vs]);
-         };
-     };
- }
+if (typeof Function.prototype.wrap === "undefined") {
+  Function.prototype.wrap = function (wrapper, ..._vs) {
+    let _method = this; //the function
+    return function (...vs) {
+      return wrapper.apply(this, [_method.bind(this), ..._vs, ...vs]);
+    };
+  };
+}
 
- function remove() {
-     let tgt = this.parentNode;
-     tgt.parentNode.removeChild(tgt);
- }
-
- function getResult(o) {
-     if (typeof o === 'function') {
-         return o();
-     }
-     return o;
- }
-
- function FF(m, funs, o) {
-     return funs[m]((f) => f(getResult(o)));
- }
-
- function EE(m, funs) {
-     return function (o) {
-         let ob = getResult(o);
-         funs[m]((f) => f(ob));
-         return ob;
-     };
- }
-
- function zip(m, funs, vals) {
-     return vals[m]((v, i) => funs[i](v));
- }
- const deferPTL = doPartial(true),
-     ptL = doPartial(),
-     //con = (v) => console.log(v),
-     compose = (...fns) => fns.reduce((f, g) => (...vs) => f(g(...vs))),
-     getter = (o, p) => o[p],
-     curry2 = fun => b => a => fun(a, b),
-     lightbox = document.querySelector('.lightbox'),
-     invoke = (f, v) => f(v),
-     invokeMethod = (o, m, v) => o[m](v),
-     lazyInvoke2 = (m, p, o, v) => o[m](p, v),
-     invokeMethodBridge = (m, v, o) => {
-         return invokeMethod(getResult(o), m, v);
-     },
-     invokeMethodBridgeCB = (cb) => (m, v, o) => {
-         return invokeMethod(cb(o), m, v);
-     },
-     getParent = curry2(getter)('parentNode'),
-     doMake = deferPTL(invokeMethod, document, 'createElement'),
-     doText = ptL(invokeMethod, document, 'createTextNode'),
-     doMakeNow = ptL(invokeMethod, document, 'createElement'),
-     getClassList = curry2(getter)('classList'),
-     getTarget = curry2(getter)('target'),
-     getAttribute = ptL(invokeMethodBridge, 'getAttribute'),
-     getParentAttribute = ptL(invokeMethodBridgeCB(getParent), 'getAttribute'),
-     setAttribute = ptL(lazyInvoke2, 'setAttribute'),
-     addListener = curry2(ptL(lazyInvoke2, 'addEventListener', 'click'))(remove),
-     setSrc = curry2(setAttribute('src')),
-     setAlt = curry2(setAttribute('alt')),
-     doOverlay = ptL(invokeMethodBridge, 'add', 'overlay'),
-     doDiv = doMake('div'),
-     doImg = doMake('img'),
-     prepend = curry2(ptL(invokeMethodBridge, 'appendChild')),
-     append = ptL(invokeMethodBridge, 'appendChild'),
-     doClose = append(doText('CLOSE')),
-     doRender = prepend(document.body),
-     /*puzzled as to why figure was being created with an extra image on evry click
-     the argument to prepend was an element and was being extended on evry call, needs a fresh instance per click*/
-     //doFig = prepend(doMakeNow('figure')),
-     doFig = prepend(doMake('figure')),
-     doCap = append(doMakeNow('figcaption')),
-     //doTest = ptL(invokeMethod, console, 'log'),
-     makeDiv = compose(doRender, doDiv),
-     git = ptL(FF, 'map', [getParentAttribute('href'), getAttribute('alt')]),
-     sit = ptL(zip, 'map', [setSrc, setAlt]),
-     enhance = compose(doOverlay, getClassList).wrap(doReturn),
-     step = compose(enhance, getParent, curry2(append)(makeDiv)),
-     fig = compose(addListener.wrap(doReturn), getParent, getParent, doClose, doCap, getParent, doFig),
-     doGit = compose(step, fig, curry2(invoke)(doImg), ptL(EE, 'forEach'), sit, git, getTarget);
- lightbox.addEventListener('click', (e) => {
-     e.preventDefault();
-     e.stopPropagation();
-     document.body.scrollTop = document.documentElement.scrollTop = 0;
-     if(e.target.src){
-        doGit(e);
-     }
- });
+const meta = nW1.meta,
+  utils = nW1.utils,
+  isFunction = meta.tagTester("Function"),
+  getResult = function (arg) {
+    if (isFunction(arg)) {
+      return arg();
+    }
+    return arg;
+  },
+  doReturn = (ptl, o) => {
+    ptl(o);
+    return o;
+  },
+  remove = function () {
+    const tgt = this.parentNode;
+    tgt.parentNode.removeChild(tgt);
+  },
+  deferPTL = meta.doPartial(true),
+  ptL = meta.doPartial(),
+  //con = (v) => console.log(v),
+  compose = meta.compose,
+  getter = meta.getter,
+  curry2 = meta.curryRight(2),
+  lightbox = document.querySelector(".lightbox"),
+  invoke = (f, v) => f(v),
+  invokeMethod = meta.invokeMethod,
+  lazyInvoke2 = (m, p, o, v) => o[m](p, v),
+  invokeMethodBridge = (m, v, o) => {
+    return invokeMethod(getResult(o), m, v);
+  },
+  invokeMethodBridgeCB = (cb) => (m, v, o) => {
+    return invokeMethod(cb(o), m, v);
+  },
+  getParent = utils.getParent,
+  doMake = deferPTL(invokeMethod, document, "createElement"),
+  doText = ptL(invokeMethod, document, "createTextNode"),
+  doMakeNow = ptL(invokeMethod, document, "createElement"),
+  getClassList = curry2(getter)("classList"),
+  getAttribute = ptL(invokeMethodBridge, "getAttribute"),
+  getParentAttribute = ptL(invokeMethodBridgeCB(getParent), "getAttribute"),
+  setAttribute = ptL(lazyInvoke2, "setAttribute"),
+  addListener = curry2(ptL(lazyInvoke2, "addEventListener", "click"))(remove),
+  setSrc = curry2(setAttribute("src")),
+  setAlt = curry2(setAttribute("alt")),
+  doOverlay = ptL(invokeMethodBridge, "add", "overlay"),
+  doDiv = doMake("div"),
+  doImg = doMake("img"),
+  prepend = curry2(ptL(invokeMethodBridge, "appendChild")),
+  append = ptL(invokeMethodBridge, "appendChild"),
+  doClose = append(doText("CLOSE")),
+  doRender = prepend(document.body),
+  /*puzzled as to why figure was being created with an extra image on every click; the argument
+  to prepend was an element and was being extended on every call, it requires a fresh instance per click*/
+  //doFig = prepend(doMakeNow('figure')),
+  doFig = prepend(doMake("figure")),
+  doCap = append(doMakeNow("figcaption")),
+  //doTest = ptL(invokeMethod, console, 'log'),
+  makeDiv = compose(doRender, doDiv),
+  git = ptL((m, fns, o) => fns[m]((f) => f(getResult(o))), "map", [
+    getParentAttribute("href"),
+    getAttribute("alt")
+  ]),
+  zip = (m, funs, vals) => vals[m]((v, i) => funs[i](v)),
+  sit = ptL(zip, "map", [setSrc, setAlt]),
+  enhance = compose(doOverlay, getClassList).wrap(doReturn),
+  step = compose(enhance, getParent, curry2(append)(makeDiv)),
+  fig = compose(
+    addListener.wrap(doReturn),
+    getParent,
+    getParent,
+    doClose,
+    doCap,
+    getParent,
+    doFig
+  ),
+  cb = (m, fns) => (o) => {
+    let ob = getResult(o);
+    fns[m]((f) => f(ob));
+    return ob;
+  },
+  doGit = compose(
+    step,
+    fig,
+    curry2(invoke)(doImg),
+    ptL(cb, "forEach"),
+    sit,
+    git
+  );
+lightbox.addEventListener("click", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
+  let el = e.target.src ? e.target : utils.getTargetNode(e.target, /img/i);
+  el && doGit(el);
+});
