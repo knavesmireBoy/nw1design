e9ca0ad9fa99be7f7f9c9eab5673d7f20a99f6d4
Andrew Sykes <andrewsykes@btinternet.com>
2022-11-23 15:19:05 +0000

    add circle listener

--- js/mob.js
+++ js/mob.js
@@ -1,14 +1,46 @@
 /*jslint nomen: true */
 /* eslint-disable indent */
-
+/*global Modernizr: false */
 /*global nW1: false */
 if (!window.nW1) {
   window.nW1 = {};
 }
+(function (Mod, brk) {
+    "use strict";
+
+      //https://webdesign.tutsplus.com/tutorials/javascript-debounce-and-throttle--cms-36783
+  //initialize throttlePause variable outside throttle function
+  function throttle(callback, time) {
+    //don't run the function if throttlePause is true
+    if (throttlePause) {
+      return;
+    }
+    //set throttlePause to true after the if condition. This allows the function to be run once
+    throttlePause = true;
+    //setTimeout runs the callback within the specified time
+    setTimeout(() => {
+      callback();
+      //throttlePause is set to false once the function has been called, allowing the throttle function to loop
+      throttlePause = false;
+    }, time);
+  }
+
+  function remove(el){
+    console.log(el)
+    let elem = utils.getRes(el);
+    console.log(elem && elem.parentNode)
+    elem && elem.parentNode.removeChild(elem);
+  }
+
 let meta = nW1.meta,
   utils = nW1.utils,
+  throttlePause,
+  pApply = meta.pApply,
+  getDesktop = pApply(Modernizr.mq, brk),
   pass = meta.pass,
+  compose = meta.compose,
   curry4 = meta.curryRight(4),
+  curry3 = meta.curryRight(3),
   ptl = meta.doPartial(1),
   setAttrs = curry4(meta.invokePair),
   setId = setAttrs("circle")("id")("setAttribute"),
@@ -18,21 +50,44 @@ let meta = nW1.meta,
   doAlt = meta.doAlternate(),
   anc = meta.$Q("main > section"),
   [img, link] = ["img", "a"].map((el) => document.createElement(el)),
-  splat = meta.compose(
+  doRemove = compose(remove, meta.$$("circle")),
+  outbound = utils.prepend(meta.$Q("main > aside")),
+  inbound = utils.prepend(anc),
+  func = ptl(meta.setterBridge(), meta.$("innerwrap"), "classList"),
+  exec = compose(func("alt"), outbound),
+  undo = compose(func(""), inbound),
+  callback = function (e) {
+    console.log(this, e)
+    e.preventDefault();
+    doAlt([exec, undo])(this);
+},
+listen = curry4(meta.invokePair)(callback)('click')('addEventListener'),
+  splat = compose(
     utils.getParent,
     setSrc.wrap(pass),
     setAlt.wrap(pass),
     utils.append(img),
+    utils.doTest,
+    listen.wrap(pass),
     setHref.wrap(pass),
     setId.wrap(pass),
     utils.append(link)
-  )(anc),
-  outbound = utils.prepend(meta.$Q("main > aside")),
-  inbound = utils.prepend(anc),
-  func = ptl(meta.setterBridge(), meta.$("innerwrap"), "classList"),
-  exec = meta.compose(func("alt"), outbound),
-  undo = meta.compose(func(""), inbound);
-splat.addEventListener("click", function (e) {
-    e.preventDefault();
-    doAlt([exec, undo])(this);
-});
+  ),
+  doMakeSplat = pApply(splat, anc),
+  isDesktop = function (alternators) {
+    if (!getDesktop()) {
+     alternators.forEach((f) => f());
+      getDesktop = pApply(meta.negate, getDesktop);
+    }
+  },
+  loaderOptions = Mod.mq(brk) ? [doRemove, doMakeSplat] : [doMakeSplat, doRemove],
+  loaderActions = doAlt(loaderOptions);
+  window.addEventListener(
+    "resize",
+    pApply(throttle, pApply(isDesktop, [loaderActions]), 222)
+  );
+  window.addEventListener('load', function() {
+    getDesktop = Mod.mq(brk) ? getDesktop : pApply(meta.negate, getDesktop);
+    loaderActions();
+  });
+}(Modernizr, "(min-width: 900px)"));
